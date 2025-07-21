50315bbd3b0af0af466ff7875c455ae20bcb8bd5
NGanch <86801593+NGanch@users.noreply.github.com>
2025-07-21 23:40:07 +0100

    Add Terraform modules for S3, VPC, and ECR

---
+++ django_docker_project/lesson-5/modules/vpc/subnets.tf
@@ -0,0 +1,21 @@
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = element(["eu-north-1a", "eu-north-1b", "eu-north-1c"], count.index)
+
+  tags = {
+    Name = "public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+  }
+}
